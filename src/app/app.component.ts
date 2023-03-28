48f8e89a9d54251d758c0252eb017b0f03b53691
EgemenCiftci <egemenciftci@gmail.com>
2023-03-28 09:15:09 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,19 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  someTextName = 'someText';
+  private _someTextName = 'someText';
 
   get someText(): string {
-    return localStorage.getItem(this.someTextName);
+    return localStorage.getItem(this._someTextName);
   }
 
   set someText(val: string) {
-    localStorage.setItem(this.someTextName, val);
+    localStorage.setItem(this._someTextName, val);
   }
 
   @HostListener('window:storage', ['$event'])
   onStorage(event: StorageEvent) {
     console.log(event);
-    this.someText = localStorage.getItem(this.someTextName);
+    this.someText = localStorage.getItem(this._someTextName);
   }
 }
